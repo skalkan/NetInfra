2f019d1181b2c1ddf742940869f0711383bfa228
Serdar Kalkan <skalkan@gmail.com>
2017-02-20 16:22:24 +0300

    Add coordinates and search to Map.cshtml

--- NetInfra.Web/Controllers/ComputersController.cs
+++ NetInfra.Web/Controllers/ComputersController.cs
@@ -175,15 +175,25 @@ namespace NetInfra.Controllers
       return View();
     }
 
-    public IActionResult Map2()
+    public IActionResult Map2(string compName = null)
     {
       List<ComputerMapViewModel> vm = new List<ComputerMapViewModel>()
       {
-        new ComputerMapViewModel { Id = 1, Name = "Computer 1", Status = "on", Lat = 30, Lon = 35},
-        new ComputerMapViewModel { Id = 2, Name = "Computer 2", Status = "off", Lat = 32, Lon = 37},
-        new ComputerMapViewModel { Id = 3, Name = "Computer 3", Status = "on", Lat = 34, Lon = 39},
-        new ComputerMapViewModel { Id = 4, Name = "Computer 4", Status = "off", Lat = 34, Lon = 39}
+        new ComputerMapViewModel { Id = 1, Name = "Computer 1", Status = "on", Lat = 38.6M, Lon = 29.4M },
+        new ComputerMapViewModel { Id = 2, Name = "Computer 2", Status = "off", Lat = 39, Lon = 35 },
+        new ComputerMapViewModel { Id = 3, Name = "Computer 3", Status = "on", Lat = 37, Lon = 36 },
+        new ComputerMapViewModel { Id = 4, Name = "Computer 4", Status = "off", Lat = 37, Lon = 36 },
+
+        new ComputerMapViewModel { Id = 5, Name = "B410-0001", Status = "on", Lat = 39.946M, Lon = 32.904M },
+        new ComputerMapViewModel { Id = 6, Name = "B410-0002", Status = "on", Lat = 39.952M, Lon = 32.906M },
+        new ComputerMapViewModel { Id = 7, Name = "S410-NET01", Status = "on", Lat = 39.946M, Lon = 32.914M },
+        new ComputerMapViewModel { Id = 8, Name = "S410-NET02", Status = "off", Lat = 39.941M, Lon = 32.917M }
       };
+
+      if (!String.IsNullOrWhiteSpace(compName))
+      {
+        vm = vm.Where(c => c.Name.Contains(compName)).ToList();
+      }
 
       //if (Request.IsAjaxRequest())
       //{
