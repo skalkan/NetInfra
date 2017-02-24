2716b400b8a784ef17cd62cfc7e2ab73584573f5
Serdar Kalkan <skalkan@gmail.com>
2017-02-24 16:00:05 +0300

    added info on hover

--- NetInfra.Web/wwwroot/js/binalar-polygon-json.js
+++ NetInfra.Web/wwwroot/js/binalar-polygon-json.js
@@ -4,7 +4,7 @@
 }, {
   "type": "LineString",
   "coordinates": [[1050, 400], [1100, 450], [1150, 550]]
-  }];
+}];
 
 var myPolygons = [{
   "type": "Polygon",
@@ -27,7 +27,41 @@ var bina_y = {
     "type": "Polygon",
     "coordinates": [
       //[[2399, 1549], [2263, 1498], [2281, 1456], [2417, 1508], [2399, 1549]]
-      [[1549, 2399], [1498, 2263], [1456, 2281], [1508, 2417], [1549,2399]]
+      [[1549, 2399], [1498, 2263], [1456, 2281], [1508, 2417], [1549, 2399]]
     ]
   }
+};
+
+var statesData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "id": "01",
+      "properties": {
+        "name": "Alabama",
+        "density": 94.65
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [[-87.359296, 35.00118], [-87.934375, 30.657966], [-88.011052, 30.685351]]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "02",
+      "properties": {
+        "name": "Alaska",
+        "density": 1.264
+      },
+      "geometry": {
+        "type": "MultiPolygon",
+        "coordinates": [
+          [[-131.602021, 55.117982], [-131.569159, 55.28229], [-132.472854, 55.780691]]
+        ]
+      }
+    }
+  ]
 };
